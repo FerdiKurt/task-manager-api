09da812854b3c85c57b96578b2f1b31250da40a0
FerdiKurtSecurrency <ferdi.kurt@securrency.com>
2020-11-11 00:54:30 +0300

    morgan package is deleted.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const morgan = require('morgan')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 
-app.use(morgan('dev'))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
